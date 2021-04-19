1f6edad1a59f8df477d438b4a502b46977257e11
Hannah Jesinkey <h.jesinkey@gmail.com>
2021-04-19 20:35:55 +0200

    completed code along

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import { Product } from './Product'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Product } from './Product';
 
 export const Products = () => {
   // TODO - fetch all products from the store
-  const allProducts = []
+  const allProducts = useSelector((store) => store.products);
 
   return (
-    <div className="products">
+    <div className='products'>
       {allProducts.map((product) => (
         <Product key={product.id} product={product} />
       ))}
     </div>
-  )
-}
+  );
+};
